e3670eecc67abfa8624f4c1c9b6c9799a89f1173
Yasir Ahmad <yahmad4@myseneca.ca>
2024-10-02 15:00:05 -0400

    Add lab3a.py with functions and pass all tests

---
+++ lab3a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# return_text_value() function
+# Author ID: yahmad4
+
+def return_text_value():
+    return "Good Morning Terry"
+
+# return_number_value() function
+def return_number_value():
+    return 15  # Change this line to match the expected output
+
+# Main Program
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
+
