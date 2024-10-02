d3aa6088e6e04741e4df377d1464e95d998b7287
Yasir Ahmad <yahmad4@myseneca.ca>
2024-10-02 15:06:53 -0400

    Add lab3b.py script for Lab 3

---
+++ lab3b.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''Lab 3 Part 1 script - functions'''
+# Author ID: [your_seneca_id] 
+
+def sum_numbers(number1, number2):
+    # Make this function add number1 and number2 and return the value
+    return number1 + number2
+
+def subtract_numbers(number1, number2):
+    # Make this function subtract number1 and number2 and return the value
+    return number1 - number2
+
+def multiply_numbers(number1, number2):
+    # Make this function multiply number1 and number2 and return the value
+    return number1 * number2
+
+if __name__ == '__main__':
+    print(sum_numbers(10, 5))
+    print(subtract_numbers(10, 5))
+    print(multiply_numbers(10, 5))
--- test_lab3.py
+++
@@ -1,8 +0,0 @@
-import lab3a
-
-# Call the return_text_value function
-text = lab3a.return_text_value()
-print(text)
-
-# Call the return_number_value function and print the result
-print(lab3a.return_number_value())
